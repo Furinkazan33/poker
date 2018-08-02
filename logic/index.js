14b45d781c5fed77d26b887ac104f1fd544a1670
Mathieu Vidalies <mathieu.vidalies@free.fr>
2018-08-02 08:18:48 +0200

    Ajout socket.io

--- logic/index.js
+++ logic/index.js
@@ -3,91 +3,78 @@ var logic = function(config) {
     /* Load default configuration file */
     if(!config) { var config = require ('./config.json'); }
 
-    this.getConfig = (cb) => cb(config)
-    this.testa = (a, cb) => cb(a)
 
-
-    this.switch = function(message) {
-
-        switch (message.id) {
-
-            /* Début de la partie */
-            case "server.game.start":
-            var player = message.data.info
-            var players = message.data.count
-                
-            break;
+        /* Début de la partie */
+        socket.on("server.game.start", function(message) {
+            this.player = message.data.info
+            this.nbPlayers = message.data.count
+        })
+    
+        /* Réception des cartes */
+        socket.on("server.game.cards", function(message) {
+            this.myCards = message.data.cards
+        })
         
-            /* Réception des cartes */
-            case "server.game.cards":
-            var cards = message.data.cards
 
-            break;
-
-            /* Une nouvelle main commence */
-            case "server.game.hand.start":
+        /* Une nouvelle main commence */
+        socket.on("server.game.hand.start", function(message) {
             // sorted by turn
-            var players = message.data.players
+            this.players = message.data.players
 
-            break;
-
-
-            /* le joueur doit indiquer son coup */
-            case "server.game.play":
-
-            break;
+        })
 
 
-            /* le joueur a mis trop de temps avant de répondre */
-            case "server.game.play.timeout":
+        /* le joueur doit indiquer son coup */
+        socket.on("server.game.play", function(message) {
 
-            break;
-
-
-            /* coup valide et pris en compte */
-            case "server.game.play.success":
-
-            break;
+        })
 
 
-            /* coup non valide et pas pris en compte */
-            case "server.game.play.failure":
+        /* le joueur a mis trop de temps avant de répondre */
+        socket.on("server.game.play.timeout", function(message) {
 
-            break;
-
-
-            /* Transmis aux autres joueurs suite à un coup valide */
-            case "server.player.action":
-            var player_id = message.data.id
-            var action = message.data.action
-
-            break;
+        })
 
 
-            /* Nouvelles cartes ajoutées sur le tapis */
-            case "server.game.board.cards":
-            var cards = message.data.cards
+        /* coup valide et pris en compte */
+        socket.on("server.game.play.success", function(message) {
 
-            break;
-
-
-            /* Une main se termine */
-            case "server.game.hand.end":
-
-            break;
+        })
 
 
-            /* Fin de la partie */
-            case "server.game.end":
-            var winner = message.data.winner
+        /* coup non valide et pas pris en compte */
+        socket.on("server.game.play.failure", function(message) {
 
-            break;
+        })
 
 
-            default:
-                break;
-        }
-    }
+        /* Transmis aux autres joueurs suite à un coup valide */
+        socket.on("server.player.action", function(message) {
+            this.player_id = message.data.id
+            this.action = message.data.action
+
+        })
+
+
+        /* Nouvelles cartes ajoutées sur le tapis */
+        socket.on("server.game.board.cards", function(message) {
+            this.cards = message.data.cards
+
+        })
+
+
+        /* Une main se termine */
+        socket.on("server.game.hand.end", function(message) {
+
+        })
+
+
+        /* Fin de la partie */
+        socket.on("server.game.end", function(message) {
+            this.winner = message.data.winner
+
+        })
+
 
 
 
@@ -97,11 +84,11 @@ var logic = function(config) {
         BET > => raise
     */
     this.action = {
-        check: function(type) { return { type: "BET", "value": 0 } },
+        check: function() { return { type: "BET", "value": 0 } },
         
-        call: function(type, value) { return { type: "BET", "value": value } },
+        call: function(value) { return { type: "BET", "value": value } },
         
-        raise: function(type, value) { return { type: "BET", "value": value } },
+        raise: function(value) { return { type: "BET", "value": value } },
 
         fold: function() { return { type: "FOLD" } },
     }
@@ -123,4 +110,4 @@ var logic = function(config) {
 }
 
 
-module.exports = logic;
+export default logic;
